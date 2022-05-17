15ad420b7d484530e8a4b8c3df6bdb43fee78a65
jhyde45 <67131895+jhyde45@users.noreply.github.com>
2022-05-18 08:37:55 +1000

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -24,6 +24,24 @@ namespace  EPSS{
     export function MoistureSensor(p: AnalogPin): number {
         return pins.map(pins.analogReadPin(p), 0, 900, 0, 100);
     }
+    /**
+    Returns the value of the moisture sensor on a scale of 0 to 100.
+    */
+    //% block="Convert number to binary"
+    export function ConvertToBinary(work: number):boolean[] {
+        let out: boolean[] = [];
+        for (let index = 0; index <= 29; index++) {
+            if (work % 2 == 1) {
+                work += -1
+                out.push(true);
+            }
+            else{
+                out.push(false);
+            }
+            work = work / 2
+        }
+        return out;
+    }
     //% blockId="error" block="test"
     //% draggableParameters="reporter" weight=0
     export function pinLow(analogReadPin: () => void) {
